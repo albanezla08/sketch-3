753700ef7879f7ebf1152a43e38b61eef5057101
Louis Albanez <102559347+albanezla08@users.noreply.github.com>
2024-03-04 11:46:51 -0600

    Create white-noise-processor.js

---
+++ white-noise-processor.js
@@ -0,0 +1,35 @@
+// white-noise-processor.js
+// from the Mozilla Docs at https://developer.mozilla.org/en-US/docs/Web/API/AudioWorkletNode/parameters
+class WhiteNoiseProcessor extends AudioWorkletProcessor {
+    static get parameterDescriptors() {
+      return [
+        {
+          name: "customGain",
+          defaultValue: 1,
+          minValue: 0,
+          maxValue: 1,
+          automationRate: "a-rate",
+        },
+      ];
+    }
+  
+    process(inputs, outputs, parameters) {
+      const output = outputs[0];
+      output.forEach((channel) => {
+        for (let i = 0; i < channel.length; i++) {
+          channel[i] =
+            (Math.random() * 2 - 1) *
+            (parameters["customGain"].length > 1
+              ? parameters["customGain"][i]
+              : parameters["customGain"][0]);
+          // note: a parameter contains an array of 128 values (one value for each of 128 samples),
+          // however it may contain a single value which is to be used for all 128 samples
+          // if no automation is scheduled for the moment.
+        }
+      });
+      return true;
+    }
+  }
+  
+  registerProcessor("white-noise-processor", WhiteNoiseProcessor);
+  
