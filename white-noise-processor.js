c669063db5bc8ea810382fd5688a04a256d77915
Louis Albanez <albanezla@uchicago.edu>
2024-03-04 14:01:45 -0600

    trying to add sine wave

--- white-noise-processor.js
+++ white-noise-processor.js
@@ -1,6 +1,12 @@
 // white-noise-processor.js
 // from the Mozilla Docs at https://developer.mozilla.org/en-US/docs/Web/API/AudioWorkletNode/parameters
 class WhiteNoiseProcessor extends AudioWorkletProcessor {
+    constructor() {
+      super();
+      this.currentValue = 0;
+      this.phase = 0;
+    }
+
     static get parameterDescriptors() {
       return [
         {
@@ -12,6 +18,10 @@ class WhiteNoiseProcessor extends AudioWorkletProcessor {
         },
       ];
     }
+
+    getValue() {
+      return this.currentValue;
+    }
   
     process(inputs, outputs, parameters) {
       const output = outputs[0];
@@ -19,10 +29,13 @@ class WhiteNoiseProcessor extends AudioWorkletProcessor {
         for (let i = 0; i < channel.length; i++) {
           channel[i] =
             // (Math.random() * 2 - 1) *
-            -1 +
-            (parameters["customGain"].length > 1
-              ? parameters["customGain"][i]
-              : parameters["customGain"][0]);
+            // -1 +
+            // (parameters["customGain"].length > 1
+            //   ? parameters["customGain"][i]
+            //   : parameters["customGain"][0]);
+            Math.sin(this.phase * 0.0576);
+          this.currentValue = Math.sin(this.phase * 0.0576);
+          this.phase++;
           // note: a parameter contains an array of 128 values (one value for each of 128 samples),
           // however it may contain a single value which is to be used for all 128 samples
           // if no automation is scheduled for the moment.
